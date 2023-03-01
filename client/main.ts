ed0727189fab2226567d1b3cc4b9a984d28dcd27
Volodymyr <volodyaruzhak@gmail.com>
2023-03-01 18:04:40 -0500

    updated with live server

--- client/main.ts
+++ client/main.ts
@@ -90,7 +90,7 @@ const handleSubmit = async (e: any) => {
     loader(messageDiv);
 
     // const response = await fetch('https://codex-im0y.onrender.com/', {
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://juliaai.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',

7f5b8ad41af7f68e4db29d95bd50d07f0e4f221f
Volodymyr <volodyaruzhak@gmail.com>
2023-02-28 21:00:47 -0500

    add some code(functions)

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 import bot from './assets/bot.svg';
 import user from './assets/user.svg';
 
-// const logo = require("./assets/bot.svg") as string;
-// const logo = require("./assets/user.svg") as string;
 
-const form = document.querySelector('form');
-const chatContainer = document.querySelector('#chat_container');
+//Typescript also has a non-null assertion that can be used when we are sure 
+//that the value is never null by adding the ! operator to the end of your statement:
+const form = document.querySelector('form')!;
+const chatContainer = document.querySelector('#chat_container')!;
 
-let loadInterval;
+let loadInterval: string | number | NodeJS.Timer | undefined;
 
 function loader(element: any) {
     element.textContent = ''
@@ -22,3 +22,108 @@ function loader(element: any) {
         }
     }, 300);
 }
+
+function typeText(element: any, text: string) {
+    let index = 0;
+
+    let interval = setInterval(() => {
+        if (index < text.length) {
+            element.innerHTML += text.charAt(index)
+            index++
+        } else {
+            clearInterval(interval)
+        }
+    }, 20)
+}
+
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
+function generateUniqueId() {
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+
+    return `id-${timestamp}-${hexadecimalString}`;
+}
+
+// type ChatStripeProps = {
+//     isAi: boolean
+//     value?: any
+//     uniqueId?: string
+// }
+
+function chatStripe(isAi: boolean, value?: any, uniqueId?: string) {
+    return (
+        `
+        <div class="wrapper ${isAi && 'ai'}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                        src=${isAi ? bot : user} 
+                        alt="${isAi ? 'bot' : 'user'}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        </div>
+    `
+    )
+}
+
+const handleSubmit = async (e: any) => {
+    e.preventDefault()
+
+    const data = new FormData(form)
+
+    // user's chatstripe
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+
+    // to clear the textarea input 
+    form.reset()
+
+    // bot's chatstripe
+    const uniqueId = generateUniqueId()
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+
+    // to focus scroll to the bottom 
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    // specific message div 
+    const messageDiv = document.getElementById(uniqueId)!;
+
+    // messageDiv.innerHTML = "..."
+    loader(messageDiv);
+
+    // const response = await fetch('https://codex-im0y.onrender.com/', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({
+    //         prompt: data.get('prompt')
+    //     })
+    // })
+
+    // clearInterval(loadInterval)
+    // messageDiv.innerHTML = " "
+
+    // if (response.ok) {
+    //     const data = await response.json();
+    //     const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+
+    //     typeText(messageDiv, parsedData)
+    // } else {
+    //     const err = await response.text()
+
+    //     messageDiv.innerHTML = "Something went wrong"
+    //     alert(err)
+    // }
+}
+
+form.addEventListener('submit', handleSubmit)
+form.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        handleSubmit(e)
+    }
+})
